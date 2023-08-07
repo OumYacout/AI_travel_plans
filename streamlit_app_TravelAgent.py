aa7a4eb5820965e5abc59d8def9f03f30e2f5097
OumYacout <139337035+OumYacout@users.noreply.github.com>
2023-08-07 14:01:54 +0100

    Update streamlit_app_TravelAgent.py

--- streamlit_app_TravelAgent.py
+++ streamlit_app_TravelAgent.py
@@ -10,7 +10,7 @@ import streamlit as st
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #or enter the api key here
-openai.api_key = self.openai_api_key
+#openai.api_key = "......"
 
 Model= "text-davinci-003"
 
